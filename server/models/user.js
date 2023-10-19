cb8a0030b95a13895b7144d04ccfc0a608176afb
Hyemin0102 <128768462+Hyemin0102@users.noreply.github.com>
2023-10-19 22:49:49 +0900

    로그인/회원가입

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,8 @@
+
 const { default: mongoose } = require("mongoose");
+const bcrypt = require("bcrypt"); // 비밀번호를 암호화 시키기 위해
+const jwt = require("jsonwebtoken"); // 토큰을 생성하기 위해
+const saltRounds = 10; // salt를 몇 글자로 할지
 
 const userSchema = mongoose.Schema({
   name:{
@@ -8,8 +12,89 @@ const userSchema = mongoose.Schema({
   password:{
     type: String,
     minlength: 5
-  }
+  },
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
 });
+
+//회원가입 시 비밀번호 hash화
+userSchema.pre("save",function(next){
+  const user = this;
+  //비밀번호 변경 시 암호화
+  if(user.isModified("password")){
+    bcrypt.genSalt(saltRounds,function(err,salt){
+      if(err)return next(err); //err있는 경우 err 발생
+      
+      //hash 생성, 생성된 salt값과 비밀번호 인자로 넘겨줌
+      bcrypt.hash(user.password,salt, function(err,hash){
+        if(err) return next(err); //err있는 경우 err 발생
+
+        //생성한 hash 값 비밀번호로 넘겨줌
+        user.password = hash;
+        next(); //save() 처리됨
+      })
+    })
+  }else{
+    next();
+  }
+},{});
+
+//인스턴스 메서드로 비밀번호 설정
+userSchema.methods.setPassword = async function(password){
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  this.password = hash;
+};
+
+//인스턴스 메서드로 비밀번호 비교
+userSchema.methods.comparePassword = async function(password){
+  const result = await bcrypt.compare(password, this.password);
+  console.log('comparePassword', password,this.password );
+  return result; //true or false
+};
+
+//스태틱 메서드로 데이터베이스에서 사용자 검색(findOne)
+userSchema.statics.findByUsername = function(name){
+  return this.findOne({name});
+};
+
+
+
+//JWT 발급
+userSchema.methods.generateToken = function(){
+  const token = jwt.sign(
+    {_id: this.id, name: this.name},//첫번째 페이로드에 넣을 데이터
+    process.env.SECRET_KEY,//개인키
+    {expiresIn:'7d'}//유효기간
+  );
+  console.log('createToken, JWT 발급!!!')
+  return token;
+}
+
+//비밀번호 확인 메소드
+/* userSchema.methods.comparePassword = function(plainPassword,cb){
+  const user = this;
+  //plainPassword암호화해서 db에 있는 비밀번호와 비교
+  bcrypt.compare(plainPassword,user.password,function(err,isMatch){
+    if(err) return cb(err);
+    cb(null, isMatch);
+  });
+}; */
+
+//토큰 생성 메서드
+/* userSchema.methods.generateToken = function(){
+  const user = this;
+  //jsonwebtoken
+  const token = jwt.sign(user._id.toHexString(),"secretToken");
+  user.token = token;
+  return user.save().then(user => user.token);
+}; */
+
+
 
 const User = mongoose.model('User',userSchema);
 module.exports = {User};
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,13 +1,123 @@
+const { User } = require('../models/user');
+const Joi = require('joi');
+const bcrypt = require("bcrypt"); 
+
 var router = require('express').Router();
 
 //회원가입 응답처리
-router.post('/register',(req,res)=>{
-  
+router.post('/register',async(req,res)=>{
+  const schema = Joi.object().keys({
+    name: Joi.string().alphanum().min(3).max(20).required(),
+    password: Joi.string().required()
+  });
+  const result = schema.validate(req.body);
+  if (result.error) {
+    res.status(400).json(result.error);
+    return;
+  }
+  //유효성 검사 성공 시
+  const { name, password } = req.body;
+  try{
+    const exists = await User.findByUsername(name);
+    if (exists) {
+      res.status(409).send('중복된 아이디가 있습니다.');
+      return;
+    }
+    const user = new User({
+      name,
+      password
+    });
+    console.log('user',user);
+    const hashPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashPassword;
+    console.log('hash---user',user);
+
+    // access_token 이름으로 쿠키에 accessToken 담고, httpOnly 활성화해 자바스크립트 쿠키 조회 방지
+    const accessToken = user.generateToken();
+
+    res.cookie('access_token', accessToken, {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true
+    });
+    res.json(user);
+    console.log('회원가입 성공', user);
+
+  }catch(err){
+res.json({success: false, err})
+  }
 });
 
 //로그인 응답처리
-router.post('/login',(req,res)=>{
-  
+router.post('/login',async(req,res)=>{
+  const { name, password } = req.body;
+  if (!name || !password) {
+    res.status(401).send('이름과 비밀번호는 필수값입니다.');
+    return;
+  }
+  try{
+    const user = await User.findByUsername(name);
+    if (!user) {
+      res.status(401).send('유효한 아이디가 아닙니다.');
+      console.log('findByUsername 같은 아이디 없음', user);
+      return;
+    }
+
+    //동일한 이름 있는 경우 비밀번호 비교
+    const valid = await bcrypt.compare(password, user.password);
+    console.log('password',password);
+    console.log('user.password',user.password);
+    if(!valid){
+      res.status(400).send('incorrect password');
+        return;
+    }
+
+    res.status(200).json({
+      loginSuccess: true,
+      user
+    })
+
+  }catch(err){
+    res.json({success: false, err})
+  }
 });
 
 module.exports = router;
+
+
+/* User.findOne({name: req.body.name})
+    .then(user=>{
+      if(!user){
+        return res.json({
+          loginSuccess: false,
+          message: "이름에 속하는 유저가 없습니다."
+        });
+      }
+      //이름이 있는 경우 비밀번호 확인
+      user.comparePassword(req.body.password)
+        .then(isMatch => {
+          if(!isMatch){
+            return res.json({
+              loginSuccess: false,
+              message: "비밀번호가 잘못되었습니다."
+            });
+          }
+          //비밀번호 맞으면 토큰 생성
+          return user.generateToken();
+        })
+        .then(user =>{
+          //토큰을 쿠키에 저장
+          res.cookie("origin_token",user.token).status(200).json({
+            loginSuccess: true,
+            userid: user._id
+          });
+        })
+        //비밀번호 확인할때 오류
+        .catch(err=>{
+          return res.status(400).send(err);
+        });
+    })
+    //findOne에서 err 처리
+    .catch(err=>{
+      return res.status(400).send(err);
+    })
+ */
